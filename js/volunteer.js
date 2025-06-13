0dda80e4afd6c6e00ec73d827dd045a3788b361d
Sanila Wijesekara <sanilamethwan@gmail.com>
2025-06-13 21:15:51 +0530

    added all the files

---
+++ js/volunteer.js
@@ -0,0 +1,14 @@
+function toggleHeart(button) {
+  button.classList.toggle('clicked');
+}
+
+window.toggleHeart = toggleHeart;
+
+window.addEventListener("scroll", function () {
+  let header = document.querySelector("header");
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
